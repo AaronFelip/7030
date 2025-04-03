c4d6f7b3dd56bde96847cb54f673dbd0c315120d
Aaron <65736788+AaronFelip@users.noreply.github.com>
2025-04-03 05:50:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
-from flask import Flask, render_template, redirect, url_for, request, g, session
-from functools import wraps
+from flask import Flask, render_template, request, g, jsonify
+from flask_json_schema import JsonValidationError, JsonSchema
+from schema import (valider_nouvel_utilisateur_schema,
+                    modifier_utilisateur_schema)
 from database import Database
+from utilisateur import Utilisateur
 
-
-regex = r"[A-Za-z0-9#$%&'*+/=?@]{8,}" #possiblement incomplet
-
-
-app = Flask(__name__, static_url_path='', static_folder='static')
-
-
-# Dans config.py ou .env pas ici
-app.secret_key = 'ebb506dc18624cb16aa029f6c20af44a'
-
-#print(secrets.token_hex(16))
-
-
-
-def connection_requise(f):
-    @wraps(f)
-    def decorated_fucntion(*args, **kwargs):
-        if 'id' not in session:
-            return redirect(url_for('login')), 302
-        return f(*args, **kwargs)
-    return decorated_fucntion
-
-
-def valider_courriel_existe(courriel):
-    return get_db().courriel_existe(courriel)
-
-
-def valider_courriel(courriel, validation_courriel):
-    return courriel == validation_courriel
-
+app = Flask(__name__)
+schema = JsonSchema(app)
 
 
 def get_db():
@@ -45,102 +20,78 @@ def get_db():
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
-        db.close_connection()
+        db.disconnect()
 
 
-@app.route('/')
-def index():
-    title = "MGL7030 PROJET SESSION"
-    return render_template('index.html',
-                           title=title), 200
+@app.errorhandler(JsonValidationError)
+def validation_error(e):
+    errors = [validation_error.message for validation_error in e.errors]
+    return jsonify({'error': e.message, 'errors': errors}), 400
 
 
-
-
-@app.route('/signin', methods=['GET', 'POST'])
-def signin():
-    title = "Mon site veuillez-vous inscrires."
-    if request.method == 'GET':
-        return render_template("sign-in.html", title=title), 200
-
-    form_data = { 'nom' : request.form.get('nom').strip(),
-                  'prenom' : request.form.get('prenom').strip(),
-                  'courriel' : request.form.get('courriel').strip(),
-                  'validation-courriel' : request.form.get('validation-courriel').strip(),
-                  'mdp' : request.form.get('mdp'),
-                  }
-    erreurs = {}
-
-    if not all(form_data.values()):
-        erreurs["message_erreur"] = "Tous les champs doivent être remplis."
-
-    if valider_courriel_existe(form_data['courriel']):
-        erreurs["courriel_erreur"] = "Ce courriel existe déjà."
-
-
+@app.route('/api/utilisateur', methods=['POST'])
+@schema.validate(valider_nouvel_utilisateur_schema)
+def utilisateur():
     try:
-        get_db().creer_utilisateur(
-            form_data['nom'],
-            form_data['prenom'],
-            form_data['courriel'],
-            form_data['mdp'],
-        )
+        data = request.get_json()
+        nouvel_utilisateur = Utilisateur(None, data["nom"], data["prenom"],
+                                         data["age"],
+                                         data["date_naissance"],
+                                         data["grades_universitaires"])
+        get_db().creer_utilisateur(nouvel_utilisateur)
+
+        """
+        201 Created : Ce code est renvoyé lorsqu'une nouvelle ressource a été 
+        créée avec succès.
+        """
+        return ("L'utilisateur " + data["prenom"] + " " +
+                    data["nom"] + " a été ajouté avec succès.", 201)
+
     except Exception as e:
-        erreurs["db_erreur"] = "Une erreur est survenu durant l'inscription"
-
-        return render_template("sign-in.html",
-                               title=title,
-                               **erreurs,
-                               **form_data), 500
-
-    return redirect(url_for("index")), 302
+        return jsonify(error="Une erreur interne s'est produite. L'erreur a été "
+                             "signalée à l'équipe de développement."), 500
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    title = "login"
-    if request.method == 'GET':
-        if 'id' in session:
-            return redirect(url_for('index'))
-        return render_template("login.html",title=title), 200
+@app.route('/api/utilisateur/<int:id>', methods=['PUT'])
+@schema.validate(modifier_utilisateur_schema)
+def modifier_utilisateur(id):
+    try:
+        data = request.get_json()
 
-    courriel = request.form.get('courriel', ' ').strip()
-    mdp = request.form.get('mdp')
+        """
+        Ici on utilisera data.get() pour qu'elle ne s'attende pas à ce que tous 
+        les champs soient toujours présents dans la requête.
+        """
+        utilisateur_modifie = Utilisateur(id,
+                                          data.get("nom"),
+                                          data.get("prenom"),
+                                          data.get("age"),
+                                          data.get("date_naissance"),
+                                          data.get("grades_universitaires"))
 
-    erreurs = {}
+        resultat = get_db().modifier_utilisateur(utilisateur_modifie)
 
-    if not courriel or not mdp:
-        erreurs['message_erreur'] = "Tous les champs"
-        return render_template("login.html",
-                               title=title,
-                               courriel=courriel,
-                               **erreurs), 400
+        if resultat:
+            return jsonify(message=f"L'utilisateur avec l'id: {id} a été modifié avec succès."), 200
+        else:
+            return jsonify(error=f"Utilisateur non trouvé avec l'id spécifié: {id}."), 404
 
-    utilisateur = get_db().verifier_utilisateur(courriel, mdp)
-
-    if utilisateur is None:
-        erreurs['message_errreur'] = "Ce courriel ou mot de passe est incvorrect"
-        return render_template("login.html",
-                               title=title,
-                               courriel=courriel,
-                               **erreurs), 401
-
-    session.clear()
-    session['id'] = utilisateur['id']
-    session['nom'] = utilisateur['nom']
-    session['prenom'] = utilisateur['prenom']
-    session['courriel'] = utilisateur['courriel']
-
-    return redirect(url_for('index')), 302
+    except Exception as e:
+        return jsonify(error="Une erreur interne s'est produite. L'erreur a été "
+                             "signalée à l'équipe de développement."), 500
 
 
 
-@app.route('/logout')
-@connection_requise
-def logout():
-    session.clear()
-    return redirect(url_for('index')), 302
+@app.route('/api/utilisateurs', methods=['GET'])
+def obtenir_utilisateurs():
+    utilisateurs = get_db().obtenir_tous_les_utilisateurs()
+    utilisateurs_json = [{"id": user.id, "nom": user.nom, "prenom": user.prenom, "age": user.age, "date_naissance": user.date_naissance, "grades_universitaires": user.grades_universitaires} for user in utilisateurs]
+    return jsonify(utilisateurs_json)
 
+
+@app.route('/doc')
+def doc():
+    return render_template("doc.html")
 
 
 if __name__ == '__main__':
